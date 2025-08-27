ea7a865d42ca04712e5b4294592b0dc6c434c513
chovin <chovin@users.noreply.github.com>
2025-08-28 09:50:30 +1000

    compress.sh work for single files also

--- compress.sh
+++ compress.sh
@@ -2,18 +2,37 @@
 
 # compress images into webp format
 # usage: ./compress.sh <input_directory> <output_directory> [quality:-75]
+# usage: ./compress.sh <input_file> [output_file/directory:-input_dir] [quality:-75]
 
-INDIR="${1}"
-OUTDIR="${2}"
+INFILE="${1}"
+OUTDIR="${2:-null}"
 QUALITY="${3:-75}"
 
-mkdir -p "$OUTDIR"
+if [[ -d $INFILE ]]; then
+    INDIR=$INFILE
+    mkdir -p "$OUTDIR"
 
-for file in "$INDIR"/*; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file")
-        name="${filename%.*}"
+    for file in "$INDIR"/*; do
+        if [ -f "$file" ]; then
+            filename=$(basename "$file")
+            name="${filename%.*}"
 
-        cwebp -q "$QUALITY" "$file" -o "$OUTDIR/${name}.webp"
+            cwebp -q "$QUALITY" "$file" -o "$OUTDIR/${name}.webp"
+        fi
+    done
+elif [[ -f $INFILE ]]; then
+    filename=$(basename "$INFILE")
+    name="${filename%.*}"
+
+    if [[ "$OUTDIR" == "null" ]]; then
+        OUTDIR=$(dirname $INFILE)
     fi
-done
+    OUTFILE=$OUTDIR
+    if [[ -d $OUTDIR ]]; then
+        OUTFILE="$OUTDIR/${name}.webp"
+    fi
+
+    cwebp -q "$QUALITY" "$INFILE" -o "$OUTFILE"
+else
+    echo "invalid path"
+fi
